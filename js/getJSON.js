e48520b84bd59280e957a69a4bc16705a092353a
Marshall Turner <turnerm5@gmail.com>
2015-04-17 15:09:49 -0700

    JSON parsing is up and running

--- js/getJSON.js
+++ js/getJSON.js
@@ -4,8 +4,24 @@ $.ajax({
 	dataType: "jsonp",
 	success: function (response) {
 		// these are stub numbers from a test API
-		$( "#saaMiles" ).append(response.results.names[0].topar)
-		$( "#paeMiles" ).append(response.results.names[4].topar)
+		
+		var saaTotal = 0;
+		var paeTotal = -35;
+
+		$.each(response.results.names, function(i, item) {
+			if (item.topar == 'E') {
+				string = '0'
+			} else {
+				string = item.topar;
+			}
+			saaTotal = saaTotal + Math.abs(parseInt(string));
+			paeTotal = paeTotal + Math.abs(parseInt(string));
+		});
+
+		$( "#saaMiles" ).append(saaTotal),
+		$( "#paeMiles" ).append(paeTotal),
+		$( "#lastupdated" ).append(response.thisversionrun)
+
 	},
 	error: function (xhr, status) {
 	//handle errors
