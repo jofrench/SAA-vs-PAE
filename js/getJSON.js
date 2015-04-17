007e6189a1c4c4f19a75968eb0540f20ee60bc40
Marshall Turner <turnerm5@gmail.com>
2015-04-17 14:35:56 -0700

    Got JSON requests up and running

--- js/getJSON.js
+++ js/getJSON.js
@@ -1,6 +1,13 @@
-$(function () {
-	$.getJSON("https://www.kimonolabs.com/api/9q9225fm?apikey=9adbc380301d558ba05315f79fff2e31", function(jsonData){
-			$("saaMiles").append(jsonData.results.names[0].topar);
-		}
-	);
+$.ajax({
+	url:"https://www.kimonolabs.com/api/9q9225fm?apikey=9adbc380301d558ba05315f79fff2e31",
+	crossDomain: true,
+	dataType: "jsonp",
+	success: function (response) {
+		// these are stub numbers from a test API
+		$( "#saaMiles" ).append(response.results.names[0].topar)
+		$( "#paeMiles" ).append(response.results.names[4].topar)
+	},
+	error: function (xhr, status) {
+	//handle errors
+	}
 });
