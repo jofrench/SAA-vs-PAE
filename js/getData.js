dcfb6c4dd03a4158585a94be458ee96c6b83efb0
Marshall Turner <turnerm5@gmail.com>
2015-04-17 16:47:49 -0700

    JSON is actually pulling from the Kimono API. Ready to start!

---
+++ js/getData.js
@@ -0,0 +1,195 @@
+$.ajax({
+	url:"https://www.kimonolabs.com/api/cqlx00zi?apikey=9adbc380301d558ba05315f79fff2e31",
+	crossDomain: true,
+	dataType: "jsonp",
+	success: function (response) {
+		// these are stub numbers from a test API
+		
+		var totalMiles = 0;
+		var totalTrips = 0;
+		var nameArray = [];
+		var milesArray = [];
+		var tripsArray = [];
+
+		$.each(response.results.leaderboard, function(i, item) {
+			totalMiles = totalMiles + Math.abs(parseInt(item.miles));
+			totalTrips = totalTrips + Math.abs(parseInt(item.trips));
+			milesArray.push(item.miles);
+			nameArray.push(item.name.text);
+			tripsArray.push(item.trips);
+		});
+
+
+		$( "#saaMiles" ).append(totalMiles);
+		$( "#saaTrips" ).append(totalTrips);
+
+		renderAngularGraph('#saaParticipation', 0);
+
+		renderLeaderBoardChart('#saaMilesLeaderboard', '#BB0000', 'Miles', nameArray, milesArray);
+		renderLeaderBoardChart('#saaTripsLeaderboard', '#BB0000', 'Trips', nameArray, tripsArray);
+
+		$( "#lastupdated" ).append(response.thisversionrun);
+
+	}
+});
+
+$.ajax({
+	url:"https://www.kimonolabs.com/api/cqlx00zi?apikey=9adbc380301d558ba05315f79fff2e31",
+	crossDomain: true,
+	dataType: "jsonp",
+	success: function (response) {
+		// these are stub numbers from a test API
+		
+		var totalMiles = 0;
+		var totalTrips = 0;
+		var nameArray = [];
+		var milesArray = [];
+		var tripsArray = [];
+
+		$.each(response.results.leaderboard, function(i, item) {
+			totalMiles = totalMiles + Math.abs(parseInt(item.miles));
+			totalTrips = totalTrips + Math.abs(parseInt(item.trips));
+			milesArray.push(item.miles);
+			nameArray.push(item.name.text);
+			tripsArray.push(item.trips);
+		});
+
+		$( "#paeMiles" ).append(totalMiles);
+		$( "#paeTrips" ).append(totalTrips);
+
+		renderAngularGraph('#paeParticipation', 0);
+
+		renderLeaderBoardChart('#paeMilesLeaderboard', '#0077D2', 'Miles', nameArray, milesArray );
+		renderLeaderBoardChart('#paeTripsLeaderboard', '#0077D2', 'Trips', nameArray, tripsArray );
+	}
+});
+
+
+
+function renderLeaderBoardChart(className, color, name, riders, miles){
+    $(className).highcharts({
+        chart: {
+            type: 'bar',
+            animation: false
+        },
+        legend: {
+            enabled: false
+        },
+        credits: {
+            enabled: false
+        },
+        title: {
+            text: ''
+        },
+        xAxis: {
+            categories: riders
+        },
+        yAxis: {
+            title: {
+                text: name
+            }
+        },
+        series: [{
+            name: name,
+            data: miles,
+            animation: false,
+            color: color
+        }]
+    });
+}
+
+function renderAngularGraph(className, percentage) {
+    var gaugeOptions = {
+
+        chart: {
+            type: 'solidgauge',
+            animation: false
+        },
+
+        title: '',
+
+        pane: {
+            center: ['50%', '70%'],
+            size: '100%',
+            startAngle: -90,
+            endAngle: 90,
+            background: {
+                backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
+                innerRadius: '60%',
+                outerRadius: '100%',
+                shape: 'arc'
+            }
+        },
+
+        tooltip: {
+            enabled: false
+        },
+
+        // the value axis
+        yAxis: {
+            stops: [
+                [0.1, '#DF5353'], // red
+                [0.5, '#DDDF0D'], // yellow
+                [0.9, '#55BF3B'] // green
+            ],
+            lineWidth: 0,
+            minorTickInterval: null,
+            tickPixelInterval: 400,
+            tickWidth: 0,
+            title: {
+                y: -70
+            },
+            labels: {
+                y: 16
+            }
+        },
+
+        plotOptions: {
+            solidgauge: {
+                dataLabels: {
+                    y: 15,
+                    borderWidth: 0,
+                    useHTML: true
+                }
+            }
+        }
+    };
+
+    // The speed gauge
+    $(className).highcharts(Highcharts.merge(gaugeOptions, {
+        yAxis: {
+            min: 0,
+            max: 100,
+            title: {
+                text: ''
+            }
+        },
+
+        credits: {
+            enabled: false
+        },
+
+        series: [{
+            animation: false,
+            name: 'Participation %',
+            data: [percentage],
+            dataLabels: {
+                format: '<div style="text-align:center"><span style="font-size:25px;color:' +
+                    ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/>' +
+                       '<span style="font-size:12px;color:silver">%</span></div>'
+            },
+            tooltip: {
+                valueSuffix: ' %'
+            }
+        }]
+
+    }));
+
+}
+
+// renderAngularGraph('#saaParticipation', 89);
+// renderAngularGraph('#paeParticipation', 65);
+// renderLeaderBoardChart('#saaMilesLeaderboard', '#C00000', 'Miles', ['Jonathan', 'Carl', 'Marshall', 'Andy', 'Adam', 'Kristina', 'Evan', 'Eric', 'Casey'], [180,135,122,101,95,85,70,65,55,40]);
+// renderLeaderBoardChart('#paeMilesLeaderboard', '#0078FF', 'Miles', ['Test 1', 'Test 2', 'Test 3', 'Test 4', 'Test 5', 'Test 6', 'Test 7', 'Test 8', 'Test 9', 'Test 10'], [160,110,90,80,65,55,43,23,10,9]);
+// renderLeaderBoardChart('#saaTripsLeaderboard', '#C00000', 'Trips', ['Marshall', 'Jonathan', 'Adam', 'Kristina', 'Carl', 'Evan', 'Andy', 'Melanie', 'Eric', 'Casey'], [25,24,20,18,14,14,12,10,9,5]);
+// renderLeaderBoardChart('#paeTripsLeaderboard', '#0078FF', 'Trips', ['Test 1', 'Test 2', 'Test 3', 'Test 4', 'Test 5', 'Test 6', 'Test 7', 'Test 8', 'Test 9', 'Test 10'], [23,23,19,16,16,14,11,7,6,4]);
